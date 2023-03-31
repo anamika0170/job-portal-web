06b5916a3f6f82afc766726bc3db56484d502f38
Anamika Rajput <anamikarajput0832@gmail.com>
2023-03-31 12:10:02 +0530

    landing page image changes done

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -3,7 +3,7 @@ import main from '../assets/images/main1.svg';
 import  '../assets/styles/landingPage.css';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
 import {Logo} from '../components';
-import { Button, Card, Container, Typography } from '@mui/material';
+import { Button, Container, Typography } from '@mui/material';
 
 
 const LandingPage = () => {
@@ -12,8 +12,8 @@ const LandingPage = () => {
    navigate('/register')
   }
   return (
-    <>
-      <main className="container">
+    <Container>
+      <main>
         <nav>
           <Logo />
         </nav>
@@ -28,12 +28,12 @@ const LandingPage = () => {
               I'm Anamika Rajput and here you can create many jobs for jobs seeker and grow your career with our Application,
               here you can create job , Edit and Delete jobs also saw your overall performance on basis of monthly activities.
             </Typography>
-            <center><Button sx={{marginTop:'20px'}}  className='img main-img btn' onClick={redirectPage}>SignIn</Button></center>
+            <Button sx={{marginTop:'20px'}} fullWidth  className='img main-img btn' onClick={redirectPage}>SignIn</Button>
           </div>
           <img src={main} alt='job hunt' className='img main-img' />
         </div>
       </main>
-    </>
+    </Container>
   );
 };
 
